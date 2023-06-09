8a0c7aeccb3a426e50a8f099a5a952d43bb2afb5
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2023-06-09 12:09:14 +0300

    move database secrets to ignored constants file

--- src/main/kotlin/data/DatabaseProvider.kt
+++ src/main/kotlin/data/DatabaseProvider.kt
@@ -5,20 +5,16 @@ import data.songs.SongsTable
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.io.File
-import java.util.*
+import constants.mysql_user
+import constants.mysql_password
 
 object DatabaseProvider {
     fun provide() {
-        val properties = Properties()
-
-        properties.load(File("local.properties").inputStream())
-
         Database.connect(
             url = "jdbc:mysql://localhost:3306/reca",
             driver = "com.mysql.cj.jdbc.Driver",
-            user = properties.getProperty("mysql_user"),
-            password = properties.getProperty("mysql_password")
+            user = mysql_user,
+            password = mysql_password
         )
 
         transaction {
